531eb8421dd38ba673e5c2330a349d43f1eb2642
Caio <caiopadilha@id.uff.br>
2022-08-29 10:43:23 -0300

    feat: tokenHelper

---
+++ src/helpers/tokenHelper.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+const JWT_CONFIG = { algorithm: 'HS256', expiresIn: '15min' };
+
+const createToken = (payload) => {
+  const token = jwt.sign(payload, process.env.JWT_SECRET, JWT_CONFIG);
+  return token;
+};
+
+const verifyToken = (token) => {
+  const dados = jwt.verify(token, process.env.JWT_SECRET);
+  return dados;
+};
+
+module.exports = { createToken, verifyToken };
