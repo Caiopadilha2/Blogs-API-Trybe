294931f4dfe1b9f8ab3bfd9df4b2fb4cff812bae
Caio <caiopadilha@id.uff.br>
2022-08-28 21:54:46 -0300

    feat: model blogPost

---
+++ src/database/models/BlogPost.js
@@ -0,0 +1,23 @@
+const BlogPost = (sequelize, DataTypes) => {
+
+    const BlogPost = sequelize.define('BlogPost', {
+      id: DataTypes.INTEGER,
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      published: DataTypes.DATE,
+      updated: DataTypes.DATE,
+      userId: DataTypes.INTEGER,
+    },
+    {
+      timestamps: false,
+    });
+
+    BlogPost.associate = (models) => {
+      BlogPost.belongsTo(models.User,
+        { foreignKey: 'userId'});
+    }
+  
+    return BlogPost;
+  };
+  
+  module.exports = BlogPost; 
