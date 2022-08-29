3b1ace365e88fd50fccf72f57bb92f7f7578aeee
Caio <caiopadilha@id.uff.br>
2022-08-28 21:55:45 -0300

    feat: model category

---
+++ src/database/models/category.js
@@ -0,0 +1,11 @@
+const Category = (sequelize, DataTypes) => {
+
+  const Category = sequelize.define('Category', {
+    id: DataTypes.INTEGER,
+    name: DataTypes.STRING,
+  });
+
+  return Category;
+};
+
+module.exports = Category;
