c61a9639e9388457a74c8b5c6a855c1e4b078bb5
Caio <caiopadilha@id.uff.br>
2022-08-30 15:21:50 -0300

    controller getbyid post

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -9,4 +9,20 @@ const getAll = async (req, res, next) => {
   }
 };
 
-module.exports = { getAll };
+const getById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const post = await postServices.getById(id);
+    if (!post) {
+      return res.status(404).json({ message: 'Post does not exist' });
+    }
+    return res.status(200).json(post);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  getAll,
+  getById,
+};
